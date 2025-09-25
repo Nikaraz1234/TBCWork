2ab5edee4a7f38dd1ebb9be2ab7cd1e93293e24d
Nikaraz1234 <77344739+Nikaraz1234@users.noreply.github.com>
2025-09-25 04:56:33 +0400

    Homework2

--- app/src/main/java/com/example/tbcworks/Main.kt
+++ app/src/main/java/com/example/tbcworks/Main.kt
@@ -1,19 +1,54 @@
 package com.example.tbcworks
 
+import kotlin.random.Random
+
+fun factorial(n: Int): Long{
+    var result = 1L
+    for(i in 1..n){
+        result *= i
+    }
+    return result
+}
+
 fun main() {
-    val mathFunctions = MathFunctions()
+    println("Start")
+    var repeat = true
+    while(repeat){
+        println("Enter first value:")
+        val x = readLine()
+        println("Now enter second value:")
+        val y = readLine()
+        println("Enter Operation Type:\n1./\n2.*\n3.%\n4.!")
+        val operation = readLine()
 
-    println("GCD of 18 and 24: ${mathFunctions.gcd(18, 24)}")
-    println("LCM of 18 and 24: ${mathFunctions.lcm(18, 24)}")
+        val xNum = x?.filter { it.isDigit() }?.toIntOrNull() ?: Random.nextInt(-127, 129)
+        val yNum = y?.filter { it.isDigit() }?.toIntOrNull() ?: Random.nextInt(-127, 129)
 
-    println("Sum of even numbers to 100: ${mathFunctions.sumOfEven()}")
+        when(operation){
+            "1", "/" -> if(yNum != 0) println("$xNum / $yNum = ${xNum / yNum}") else println("Cannot divide by zero")
+            "2", "*" -> println("$xNum * $yNum = ${xNum * yNum}")
+            "3", "%"-> if(yNum != 0) println("$xNum % $yNum = ${xNum % yNum}") else println("Cannot modulo by zero")
+            "4", "!"->{
+                val ans = xNum / yNum
+                if(ans >= 0) println("($xNum / $yNum)! = ${factorial(ans)}")
+                else println("Factorial not defined for negative numbers")
+            }
+            else -> throw IllegalArgumentException("Wrong Operation")
+        }
+        println("Do you want to repeat?\ny - Yes\nn - No")
+        var answer = readLine()
+        if(answer.equals("n", ignoreCase = true)){
+            repeat = false
+        }else if(answer.equals("y", ignoreCase = true)){
+            repeat = true
+        }else{
+            throw IllegalArgumentException("Wrong Answer!")
+        }
+        println("End")
+    }
 
-    println(mathFunctions.isPolyndrom("lalalal"))
-    println(mathFunctions.isPolyndrom("slalalal"))
 
-    println(mathFunctions.containSymbol("dsadda$"))
-    println(mathFunctions.containSymbol("dsadda"))
 
-    val number = 10220
-    println("Reversed number of $number: ${mathFunctions.reverseNumber(number)}")
+
+
 }
